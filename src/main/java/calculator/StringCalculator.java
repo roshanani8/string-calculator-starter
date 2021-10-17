0c3e1892b9c2470290fcdc4d27fbf157e39e4971
roshanani8 <84777576+roshanani8@users.noreply.github.com>
2021-10-17 12:19:36 +0530

    Update StringCalculator.java
    first commit

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,24 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	 public int add(String input) {
+		 String[] numbers=input.split(",");    
+		 if(isEmpty(input))
+		 {
+		 	return 0;
+		 }
+		 if(input.length()==1) {
+		 	return stringToInt(input);
+		 }
+		 else {
+		 	return Integer.parseInt(numbers[0]+Integer.parseInt(numbers[1]));
+		 }
 
+		 }
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+private int stringToInt(String input) {
+	return Integer.parseInt(input);
+}
 }
