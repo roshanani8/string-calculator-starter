f027df17d4b20e28d94cfe00b4a152d772a6051d
roshanani8 <84777576+roshanani8@users.noreply.github.com>
2021-10-17 12:32:17 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -9,26 +9,55 @@ import org.junit.jupiter.api.Test;
 public class StringCalculatorShould {
 	
 	
-	private StringCalculator stringCalculator;
-	
-	@Before
-	public void init() {
-	stringCalculator=new StringCalculator() ;
-	}
-
-	@Test
-   public void empty_string_should_return_0() {
+private StringCalculator stringCalculator;
+    @Test
+    public void emptyStringShouldReturnZero() {
+    
+        assertEquals(0, stringCalculator.add(""));
+    }
+    @Test
+    public void oneNumberShouldReturnItseft() {
+        
+        assertEquals(3, stringCalculator.add("3"));
+    }
+    @Test
+    public void twoNumbersShouldBeAdded() {
+        
+        assertEquals(7, stringCalculator.add("2,5"));
+    }
+    @Test
+    public void moreDigitsSupported() {
+        
+        assertEquals(77, stringCalculator.add("22,55"));
+    }
+ 
+    @Test
+    public void allowNnumersAsInput() {
+        
+        assertEquals(45, stringCalculator.add("1,2,3,4,5,6,7,8,9"));
+    }
+    // p3 ------------------
+    @Test
+    public void supportNewLineAsSeparator() {
        
-        assertEquals(0,stringCalculator.add(""));
+        assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 
-  @Test
-    void string_with_single_number_should_return_number_as_int() {
-       
-        assertEquals(1,stringCalculator.add("1"));
+    @Test
+    public void supportForCustomDelimiter() {
+        
+        assertEquals(10, stringCalculator.add(";\n1;2;3;4"));
     }
-  @Test
-  public void twoNumbersCommaDelimitedReturnSum() {
-	  assertEquals(stringCalculator.add("1,2"), 3);
-  }
+
+    @Test
+    public void negativeNotSupported() {
+        StringCalculator sut = new StringCalculator();
+        try {
+        	stringCalculator.add("-1;4");
+            fail("exception should have been thrown");
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("negatives not allowed -1", e.getMessage());
+        }
+    }
 }
